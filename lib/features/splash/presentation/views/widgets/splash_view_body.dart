55a636a4cfef575eac9cdbe80b6fcd20bcf67425
Messaoud Benkouider <massoudsa55@gmail.com>
2025-05-17 17:50:21 +0100

    create splash view

---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+   SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(children: []));
+  }
+}
