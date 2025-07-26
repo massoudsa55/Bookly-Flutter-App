27cf76360f27480bd4dafcf783cf5b89d9bf78a5
Messaoud Benkouider <massoudsa55@gmail.com>
2025-07-26 16:06:42 +0100

    create FeaturedBooks Methode in implement repo

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -32,8 +32,24 @@ class HomeRepoImpl implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      final response = await apiService.get(
+        endpoint: 'volumes?Filtering=free-ebooks&q=subject:programming',
+      );
+
+      final books =
+          (response['items'] as List)
+              .map((item) => BookModel.fromJson(item))
+              .toList();
+      return Right(books);
+    } catch (e) {
+      // return Left(ServerFailure());
+      if (e is DioException) {
+        return Left(ServerFailure.fromDioError(e));
+      }
+      return Left(CacheFailure(e.toString()));
+    }
   }
+  
 }
