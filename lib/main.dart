1f8f1af43cf8b950f8c5489c12a89d7a3eec7ea6
Messaoud Benkouider <massoudsa55@gmail.com>
2025-07-28 21:25:25 +0100

    Featured Books Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
-import 'package:bookly_flutter_app/core/utils/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'constants.dart';
 import 'core/utils/app_router.dart';
+import 'core/utils/service_locator.dart';
 import 'features/home/presentation/manager/featured_books/featured_books_cubit.dart';
 import 'features/home/presentation/manager/newest_books/newest_books_cubit.dart';
 
